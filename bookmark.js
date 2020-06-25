dff78b5e196b79793542321d6a01244623320bd3
Lucas Han <ldhan0715@gmail.com>
2020-06-25 12:56:44 +0900

    Update Source Code

--- bookmark.js
+++ bookmark.js
@@ -1,19 +1,66 @@
 const body = document.querySelector("body");
-const bookmarkTab = document.querySelector("#bookmark");
-const bookmarkButton = bookmarkTab.querySelector("button");
+const bookmarkTab = document.querySelector("#addBookmark");
 const overlay = document.querySelector("#overlay");
-const bookmarkExit = overlay.querySelector("#exit");
+const buttons = document.querySelector("#buttons");
+const closeButton = buttons.querySelector("#cancel");
+const bookmarkCreate = overlay.querySelector("#create");
 
-function handleBookmarkAdd(event) {
+const BOOKMARK_LS = "bookmark";
+
+const bookmarks = [];
+
+function saveBookmark() {
+    localStorage.setItem(BOOKMARK_LS, JSON.stringify(bookmarks));
+}
+
+function addBookmark(url,name) {
+    const link = document.createElement("a");
+    link.href = url;
+    link.innerText = name;
+    link.classList.add("bookmark");
+    // body.appendChild(link);
+    const bookmarkObject = {
+        url: url,
+        name: name
+    }
+    bookmarks.push(bookmarkObject);
+    saveBookmark();
+    hideOverlay();
+}
+
+function showOverlay(event) {
     event.preventDefault();
     overlay.style.transform = "translateY(-90vw)";
 }
 
+function hideOverlay() {
+    overlay.style.transform = "translateY(90vw)";
+}
+
+function handleBookmarkCreate(event) {
+    event.preventDefault();
+    const url = bookmarkCreate.querySelector("#url").value;
+    const name = bookmarkCreate.querySelector("#name").value;
+    addBookmark(url,name);
+}
+
+function loadBookmark() {
+    const loadedBookmarks = localStorage.getItem(BOOKMARK_LS);
+    if (loadedBookmarks !== null) {
+        const parsedBookmarks = JSON.parse(loadedBookmarks);
+        parsedBookmarks.forEach(
+            function(bookmark) {
+                addBookmark(bookmark.url, bookmark.name);
+            }
+        )
+    }
+}
+
 function init() {
-    bookmarkTab.addEventListener("submit", handleBookmarkAdd);
-    bookmarkExit.addEventListener("click", function() {
-        overlay.style.transform = "translateY(90vw)";
-    });
+    loadBookmark();
+    bookmarkTab.addEventListener("submit",showOverlay);
+    closeButton.addEventListener("click",hideOverlay);
+    bookmarkCreate.addEventListener("submit", handleBookmarkCreate);
 }
 
 init();
